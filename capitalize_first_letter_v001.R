96e26b8ad78f74a9bf2392c415f61fd5010e1233
Craig W. Slinkman <cwslinkman@gmail.com>
2019-12-04 10:26:16 -0600

    2019-12-04 10:23
    1. Add functiobn
       capitalise_all_first_letters
    
    2. Revised argument name in
       capialize_first_letter

--- capitalize_first_letter_v001.R
+++ capitalize_first_letter_v001.R
@@ -1,19 +1,41 @@
 # capitalize_first_letter.r
 
-#' function capitalize_first_letter
-#'
-#' @param y is an input string  
+#' function capitalize_first_letter( string )
+#' This functtions captialises the first letter of a string.  It was
+#' orginally written by Dr. Arvind Singla
+#' 
+#' @param string is an input string  
 #'
 #' @return the input string with the first letter caapitalized
 
 #'
 #' @examples
 #' 
-capitalize_first_letter <- function(y) 
+capitalize_first_letter <- function( string ) 
     {
-       c <- strsplit(y, " ")[[1]]
+       c <- strsplit( string, " ")[[1]]
        paste(toupper(substring(c, 1,1)), substring(c, 2),
           sep="", collapse=" ")
 }
 
+capitalize_all_first_letters <- function( strings )
+{
+    map_chr( strings, capitalize_first_letter )
+              
+}
+
+###############################################################################
+# Tests
+# Remove "#'s" to run them.
+#
+# names <- c( "peyton",
+#             "Ela", 
+#             "samantha",
+#             "siah",
+#             "melody" )
+# names
+# 
+# capitalize_first_letter( names )
+# 
+# capitalize_all_first_letters( names )
 
